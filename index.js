0761057b17bf050bf3454ff55d434f7cbd9aee3c
Toivonen <nuutti.toivonen@if.fi>
2022-04-06 10:31:59 +0300

    upload initial

---
+++ index.js
@@ -0,0 +1,71 @@
+// document.querySelectorAll('button').forEach(addEventListener("click", handleClick));
+
+var drumButtonList = document.querySelectorAll('.drum');
+
+for (var i=0; i < drumButtonList.length; i++) {
+    drumButtonList.item(i).addEventListener('click', function() {
+        makeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+document.addEventListener('keydown', function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(drumKey) {
+    switch (drumKey) {
+        case 'b':
+             var kick = new Audio('sounds/kick-bass.mp3');
+             kick.play();
+         break;
+ 
+         case 's':
+             var snare = new Audio('sounds/snare.mp3');
+             snare.play();
+         break;
+ 
+         case 'r':
+             var crash = new Audio('sounds/crash.mp3');
+             crash.play();
+         break;
+ 
+         case 't':
+             var tom1 = new Audio('sounds/tom-1.mp3');
+             tom1.play();
+         break;
+ 
+         case 'y':
+             var tom2 = new Audio('sounds/tom-2.mp3');
+             tom2.play();
+         break;
+ 
+         case 'u':
+             var tom3 = new Audio('sounds/tom-3.mp3');
+             tom3.play();
+         break;
+ 
+         case 'k':
+             var tom4 = new Audio('sounds/tom-4.mp3');
+             tom4.play();
+         break;
+ 
+         default: console.log(drumKey);
+    }
+}
+
+function buttonAnimation(drumKey) {
+    try {
+        var activeButton = document.querySelector('.' + drumKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");}, 100);
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+   
+
+// alert("hello world!");
